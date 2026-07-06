4d526990eba694d05aa1883d5e548b6e0f0c310f
anan <ananomar51@gmail.com>
2026-07-06 14:21:37 +0300

    fix: resolve failing tests by completing user factory attributes and registering product resource create/edit routes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,11 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'phone' => fake()->phoneNumber(),
+            'loyalty_points_balance' => 0,
+            'referral_code' => strtoupper(Str::random(8)),
+            'referred_by_id' => null,
+            'is_active' => true,
         ];
     }
 
