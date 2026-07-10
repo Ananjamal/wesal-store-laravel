20c607bc442181bda066829db36e0dfd59bc499b
anan <ananomar51@gmail.com>
2026-07-10 13:43:51 +0300

    design: adjust brand logo size to prevent sidebar distortion

--- resources/views/filament/brand.blade.php
+++ resources/views/filament/brand.blade.php
@@ -3,7 +3,7 @@
     $storeName = \App\Models\StoreSetting::getValue('store_name', 'وِصال');
 @endphp
 <div class="flex items-center gap-3 py-1">
-    <div class="relative flex h-14 w-14 shrink-0 overflow-hidden rounded-xl border border-[#D1CBB4] bg-white shadow-sm transition hover:scale-105">
+    <div class="relative flex h-11 w-11 shrink-0 overflow-hidden rounded-xl border border-[#D1CBB4] bg-white shadow-sm transition hover:scale-105">
         <img src="{{ $logo ? asset('storage/' . $logo) : asset('images/logo.jpg') }}" alt="Logo" class="h-full w-full object-cover">
     </div>
     <div class="flex flex-col">
